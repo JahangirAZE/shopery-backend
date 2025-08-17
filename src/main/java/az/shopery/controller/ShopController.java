c6a42d49ce83d78f2d2b777f4fac2af8a724a5cc
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2025-08-17 22:21:00 +0400

    bug fix

--- src/main/java/az/shopery/controller/ShopController.java
+++ src/main/java/az/shopery/controller/ShopController.java
@@ -1,16 +1,17 @@
 package az.shopery.controller;
 
-import az.shopery.model.dto.response.ShopResponseDto;
 import az.shopery.model.dto.response.SuccessResponseDto;
 import az.shopery.model.dto.response.UserShopResponseDto;
 import az.shopery.service.ShopService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/shops")
@@ -26,17 +27,16 @@ public class ShopController {
     }
 
     @GetMapping
-    public ResponseEntity<SuccessResponseDto<Page<ShopResponseDto>>> getAllShops(Pageable pageable) {
+    public ResponseEntity<?> getAllOrSearchShops(
+            @RequestParam(required = false) String id,
+            @RequestParam(required = false) String name,
+            Pageable pageable) {
+        if (id != null) {
+            return ResponseEntity.ok(shopService.getShopById(id));
+        }
+        if (name != null && !name.isBlank()) {
+            return ResponseEntity.ok(shopService.getShopByShopName(name));
+        }
         return ResponseEntity.ok(shopService.getAllShops(pageable));
-    }
-
-    @GetMapping
-    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopById(@RequestParam String id) {
-        return ResponseEntity.ok(shopService.getShopById(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopByShopName(@RequestParam String name) {
-        return ResponseEntity.ok(shopService.getShopByShopName(name));
     }
 }
